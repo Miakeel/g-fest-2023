95e255405c01af115af78dcffeb71998fa2d2617
Miakeel <mihaimotoroi@gmail.com>
2023-08-14 15:17:51 +0300

    final code

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoApp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rock_n_iasi_tracking.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
